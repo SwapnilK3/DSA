8538ea489f866f78a1aa3abde9745b397a0b72dd
= <=>
2025-11-21 00:41:12 +0530

    optimized prime nu,ber logiv to sqrt(n)

--- cpp/prime.cpp
+++ cpp/prime.cpp
@@ -1,8 +1,9 @@
-#include "./base.h"
+#include "./alias.h"
 
 bool is_prime_num(int n){
     bool is_prime = true;
-    for(int i=2; i<n; i++){
+    for(int i=2; i<=sqrt(n); i++){
+
         if (n%i==0){
             return false;
         }
