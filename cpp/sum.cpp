7e2c139b4b8ccc5cec4b57dab44ecef6046da393
= <=>
2025-11-20 22:35:03 +0530

    started cpp dsa with summation

---
+++ cpp/sum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#define co cout
+int sumation(int n){
+    int i = 1;
+    int sum=0;
+    while (i <= n){
+        sum += i;
+        i++;
+    }
+    return sum;
+}
+int main(){
+    int sum;
+    int n;
+    co << "Add Number for having Summation value:";
+    cin >> n;
+    sum = sumation(n);
+    co << sum;
+    return 0;
+}
